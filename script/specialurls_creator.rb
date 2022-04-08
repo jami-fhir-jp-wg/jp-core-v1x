c6aa6c3afb1244417374b8f241517d8c4bf9b516
Riki Miyakawa <riki.miyakawa@findex.co.jp>
2022-04-08 12:46:56 +0900

    special_urlを直接編集するように変更

--- script/specialurls_creator.rb
+++ script/specialurls_creator.rb
@@ -1,33 +1,54 @@
 require "json"
 genenareted = "./fsh-generated/resources/"
-work = "./temp/scriptwork/"
-linkpage= work + "specialurls.txt"
+sushiconfigPath="sushi-config.yaml"
 
 #--- URL抽出処理 ---
-def putUrl(prefix, fw, extension = false)
-    files = Dir.glob(prefix + "-*.json")
-    for fl in files
-        File.open(fl) do |f|
-            h = JSON.load(f)
-            if (h["type"] == "Extension") == extension then
-              fw.puts "    - " + h["url"].to_s
-            end
-        end
-    end
+def fillUrl(prefix, sb, extension = false)
+  files = Dir.glob(prefix + "-*.json")
+  for fl in files
+      File.open(fl) do |f|
+          h = JSON.load(f)
+          if (h["type"] == "Extension") == extension then
+            sb << "    - " + h["url"].to_s + "\n"
+          end
+      end
+  end
 end
 
-begin
-  if !Dir.exists?(work) then
-    Dir.mkdir(work)
+# configの読み込み
+def getConfingText(path)
+  File.open(path, 'r') do |fr|
+    return fr.read()
   end
-  File.open(linkpage,"w") do |fw|
-    Dir.chdir(genenareted)
-    #プロパティ名追加
-    fw.puts  "  special-url:"
-    #URL出力
-    putUrl("StructureDefinition", fw)
-    putUrl("StructureDefinition", fw, true)
-    putUrl("CapabilityStatement", fw)
-    putUrl("SearchParameter", fw)
-  end 
+end
+
+# configの書き込み
+def setConfigText(path, text)
+  File.open(path, 'w') do |fw|
+    fw.write(text)
+  end
+end
+
+#==============================================
+# メイン処理（エントリポイント）
+#==============================================
+begin
+  sb = "  special-url:\n"
+  
+  Dir.chdir(genenareted)  ##fshgenerated/resoucesに移動
+  #プロパティ名追加
+  
+  #URL出力
+  fillUrl("StructureDefinition", sb)
+  fillUrl("StructureDefinition", sb, true)
+  fillUrl("CapabilityStatement", sb)
+  fillUrl("SearchParameter", sb)
+
+  Dir.chdir('../..')  ##project配下に戻す
+  allText = getConfingText(sushiconfigPath)
+  
+  regex = /\s{2}special-url:\n(\s{4}-\shttp.*\n)*/
+  allText.gsub!(regex, sb)
+
+  setConfigText(sushiconfigPath, allText)
 end
