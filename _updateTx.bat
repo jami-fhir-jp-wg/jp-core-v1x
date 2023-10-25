b897e02670afadf9139825d5db900e8b932ee959
Riki Miyakawa <riki.miyakawa@gmail.com>
2023-10-25 13:24:33 +0900

    パッチファイル名称変更

--- _updateTx.bat
+++ _updateTx.bat
@@ -1,17 +1,40 @@
 SET fhir_dir=%SystemDrive%\\USERS\\%USERNAME%\\.fhir
+SET temp_dir=.\temp
 ECHO %fhir_dir%
 IF NOT EXIST %fhir_dir% (
     MKDIR %fhir_dir%
 )
 
-REM package download
+IF NOT EXIST %temp_dir% (
+    MKDIR %temp_dir%
+)
+
+IF NOT EXIST %fhir_dir%\packages (
+    GOTO download_package
+)
+SET /p clear="clear packeges?  (resouce and terminology) (Y/N) "
+IF %clear:Y=Y%==Y (
+    RMDIR /S /Q %fhir_dir%\packages
+)
+
+:download_package
+SET /p dpkg="download packeges of resource? (Y/N) "
+IF NOT %dpkg:Y=Y%==Y (
+    GOTO download_terminology
+)
+
 SET dlpkgurl=https://jpfhir.jp/fhir/fhir_dotFHIR_packages.tgz
 SET localpkg=.\\temp\\fhir_dotFHIR_packages.tgz
 
-REM CURL %dlpkgurl% > %localpkg%
+CURL %dlpkgurl% > %localpkg%
 tar -xvzf %localpkg% -C %fhir_dir%
 
-REM terminlogy download
+:download_terminology
+
+SET /p dterm="download packeges of terminology? (Y/N) "
+IF NOT %dterm:Y=Y%==Y (
+    GOTO end
+)
 SET dltermurl=https://jpfhir.jp/fhir/core/terminology/jpfhir-terminology-1.1.1.tgz
 SET localterm=.\\temp\\jpfhir-terminology-1.1.1.tgz
 set term_dir=%fhir_dir%\\packages\\jpfhir-terminology#1.1.1
@@ -24,4 +47,5 @@ IF NOT EXIST %term_dir% (
 
 tar -xvzf %localterm% -C %term_dir%
 
+:end
 pause
