46144b14406b4d0bfd2d9637053ee8c3d0aed528
Riki Miyakawa <findex-miyakawa>
2024-11-06 18:30:41 +0900

    日本語を指定したビルド、警告の日本語化対応

--- _genonce.bat
+++ _genonce.bat
@@ -3,16 +3,10 @@ SET publisher_jar=publisher.jar
 SET input_cache_path=%CD%\input-cache
 
 ECHO Checking internet connection...
-PING tx.fhir.org -4 -n 1 -w 1000 | FINDSTR TTL && GOTO isonline
 ECHO We're offline...
+SET javaoption=-Duser.language=ja -Duser.country=JP -Djava.awt.headless=true
+@REM SET javaoption= -Xmx12G -Duser.language=ja -Duser.country=JP -Djava.awt.headless=true
 SET txoption=-tx n/a
-GOTO igpublish
-
-:isonline
-ECHO We're online
-SET txoption=
-
-:igpublish
 
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
