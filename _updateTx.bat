40ffbdf9d9c49ee7f612464171fe89e995fd8d47
kazuhiko-ohe <kohe@m.u-tokyo.ac.jp>
2024-12-26 11:28:38 +0900

    r4-1.2.4-url->r4-1.3.0

--- _updateTx.bat
+++ _updateTx.bat
@@ -35,8 +35,8 @@ SET /p dterm="download packeges of terminology? (Y/N) "
 IF NOT %dterm:Y=Y%==Y (
     GOTO end
 )
-SET dltermurl=https://jpfhir.jp/fhir/core/terminology/jpfhir-terminology.r4-1.2.4-url.tgz
-SET localterm=.\\temp\\jpfhir-terminology.r4-1.2.4-url.tgz
+SET dltermurl=https://jpfhir.jp/fhir/core/terminology/jpfhir-terminology.r4-1.3.0.tgz
+SET localterm=.\\temp\\jpfhir-terminology.r4-1.3.0.tgz
 set term_dir=%fhir_dir%\\packages\\jpfhir-terminology.r4#1.2.4-url
 
 CURL %dltermurl% > %localterm%
